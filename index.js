afdb28bbfaf0780ba4a0a10e3427188df61ad1f5
Abe <abesell132@gmail.com>
2021-12-04 22:06:09 -0500

    asdf

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { loadBaseImage, getLabeledTrainingPixels } = require("./training");
 const utils = require("./utils");
-const tf = require("@tensorflow/tfjs-node-gpu");
+// const tf = require("@tensorflow/tfjs-node-gpu");
 
 async function start() {
-  await console.clear();
-  await console.log("Loading Base Image...");
+  await console.time("Loading Base Image...");
   const baseImage = await loadBaseImage();
-  await console.clear();
-  await console.log("Retrieving Labeled Training Data...");
-  const labeledTrainingPixels = await getLabeledTrainingPixels();
+  await console.timeEnd("Loading Base Image...");
 
-  await console.clear();
-  await console.log("Parsing Training Data...");
+  await console.time("Retrieving Labeled Training Data...");
+  const labeledTrainingPixels = await getLabeledTrainingPixels();
+  await console.timeEnd("Retrieving Labeled Training Data...");
+
+  await console.time("Parsing Training Data...");
   const numTrainingPoints = 10000;
   let colors = [];
   let labels = [];
@@ -23,21 +23,22 @@ async function start() {
     await labels.push(labelList.indexOf(trainingPixel.label));
     await colors.push(trainingPixelNeighbors);
   }
+  await console.timeEnd("Parsing Training Data...");
 
   if (process.env.NODE_ENV === "proxmox") {
     const { createModel, trainModel } = require("./neuralNetwork");
 
-    await console.clear();
-    await console.log("Creating Model...");
+    await console.time("Creating Model...");
     const model = await createModel();
     let inputs = await tf.tensor2d(colors);
     let labelsTensor = await tf.tensor1d(labels, "int32");
     let outputs = await tf.oneHot(labelsTensor, 4).cast("float32");
     await labelsTensor.dispose();
+    await console.timeEnd("Creating Model...");
 
-    await console.clear();
-    await console.log("Training model...");
+    await console.time("Training model...");
     await trainModel(model, inputs, outputs);
+    await console.timeEnd("Training model...");
   }
 }
 
