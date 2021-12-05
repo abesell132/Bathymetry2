c3364c4f3e27899770e06a47a42799a360ef627a
Abe <abesell132@gmail.com>
2021-12-05 17:28:04 -0500

    asdf

--- neuralNetwork/feedToNeuralNetwork.js
+++ neuralNetwork/feedToNeuralNetwork.js
@@ -253,7 +253,6 @@ module.exports = feedToNeuralNetwork = async (lakeImages) => {
           if (process.env.NODE_ENV == "proxmox") {
             const tf = require("@tensorflow/tfjs-node-gpu");
             const model = await tf.loadLayersModel(`file://${path.join(__dirname, "../model/model.json")}`);
-            await console.log("asdf");
             await tf.tidy(() => {
               const input = tf.tensor2d([
                 [
