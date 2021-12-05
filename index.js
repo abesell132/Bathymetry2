545cf40875302c687b21d0666dd64c764e9921ab
Abe <abesell132@gmail.com>
2021-12-04 21:52:21 -0500

    asdf

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const { loadBaseImage, getLabeledTrainingPixels } = require("./training");
 const utils = require("./utils");
 
 async function start() {
+  await console.clear();
+  await console.log("Loading Base Image...");
   const baseImage = await loadBaseImage();
+  await console.clear();
+  await console.log("Retrieving Labeled Training Data...");
   const labeledTrainingPixels = await getLabeledTrainingPixels();
 
+  await console.clear();
+  await console.log("Parsing Training Data...");
   const numTrainingPoints = 10000;
   let colors = [];
   let labels = [];
@@ -20,12 +26,16 @@ async function start() {
   if (process.env.NODE_ENV === "proxmox") {
     const { createModel, trainModel } = require("./neuralNetwork");
 
+    await console.clear();
+    await console.log("Creating Model...");
     const model = await createModel();
     let inputs = await tf.tensor2d(colors);
     let labelsTensor = await tf.tensor1d(labels, "int32");
     let outputs = await tf.oneHot(labelsTensor, 4).cast("float32");
     await labelsTensor.dispose();
 
+    await console.clear();
+    await console.log("Training model...");
     await trainModel(model, inputs, outputs);
   }
 }
